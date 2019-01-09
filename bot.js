f34048d71f84d3ee4d366da194154dad77235168
vatanstore <45723890+vatanstore@users.noreply.github.com>
2019-01-09 06:49:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,32 @@ client.on('ready', () => {
 });
 
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+   client.user.setActivity("",{type: 'WATCHING'})
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+ 
 
  
 client.login(process.env.BOT_TOKEN);
