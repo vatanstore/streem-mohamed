2d3c43e3b186161682c2fefea74f9b962eefc5dd
vatanstore <45723890+vatanstore@users.noreply.github.com>
2019-01-09 06:56:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
-client.on('ready', () => {
-   console.log(`bot online`);
-   client.user.setGame(`لا تحزن إذا حذفك أحدهم من حياته، فلربما لم يتحمّل مرافقة الملوك، فقرّر المشي مع أمثاله.`,"http://twitch.tv/.Mohamed")
-  client.user.setStatus("dnd")
-});
 
 
 client.on('message', message => {
